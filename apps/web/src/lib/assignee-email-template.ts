c768484c861799352dd18f28784ba7413995ac03
Kang, Donguk <kangdongouk@gmail.com>
2025-12-30 15:27:28 +0900

    fix: align email template format

--- apps/web/src/lib/assignee-email-template.ts
+++ apps/web/src/lib/assignee-email-template.ts
@@ -1,5 +1,5 @@
 // Trace: spec_id=SPEC-worknote-email-copy-001 task_id=TASK-0071
 
 export function buildAssigneeEmailTemplate(assigneeName: string): string {
-  return `안녕하세요. ${assigneeName} 선생님, 교육정보원 강동욱입니다.\n\n\n강동욱 드림.`;
+  return `안녕하세요. ${assigneeName} 선생님, 교육정보원 강동욱입니다.\n\n강동욱 드림.`;
 }
